c3a9470b708067ad6286b6e2d1f36982fa180512
mountassirJerrari <105740691+mountassirJerrari@users.noreply.github.com>
2025-04-27 20:28:35 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,10 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.8ff8fbf05e8240a9bdc6568cd96962a8',
+  appId: 'com.lovable.dreamwhisper',
   appName: 'dream-whisper-chat-journal',
   webDir: 'dist',
-  server: {
-    url: 'https://8ff8fbf0-5e82-40a9-bdc6-568cd96962a8.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+ 
   ios: {
     contentInset: 'always'
   },
